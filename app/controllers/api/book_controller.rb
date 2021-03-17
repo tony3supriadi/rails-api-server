eca6542c54c37d10a6e56bfa61e682657a52fe35
Toni Tri Supriadi <tony3supriadi@gmail.com>
2021-03-17 22:32:42 +0700

    membuat data buku baru

--- app/controllers/api/book_controller.rb
+++ app/controllers/api/book_controller.rb
@@ -6,7 +6,32 @@ class Api::BookController < ApplicationController
         @books = Book.all
         render json: { 
             message: "success",
-            results: @book
+            results: @books
         }, status: :ok
     end
+
+    # Membuat data buku baru
+    # POST: /api/books
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            render json: {
+                message: 'success',
+                result: @book
+            }, status: :created
+        else
+            render json: {
+                message: 'failed',
+                result: null,
+            }, status: :bad_request
+        end
+    end
+
+    # Private method untuk kebutuhan controllers
+    private 
+
+        # Inisialisasi data field table buku
+        def book_params
+            params.permit(:title, :author, :description)
+        end
 end
