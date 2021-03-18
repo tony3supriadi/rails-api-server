17a13ba986c9faab01b53f44c50feee8d6276ff2
Toni Tri Supriadi <tony3supriadi@gmail.com>
2021-03-18 11:40:29 +0700

    cors origin fixed

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module RailsServer
     # CORS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000'
+        origins '*'
         resource '*', :headers => :any, :methods => :any
       end
     end
